d080f1e17ed2c1fe16a74de33ad0da83247fc774
César de Moura <84753451+eucesarmoura@users.noreply.github.com>
2023-11-21 05:35:26 -0300

    Código JS para animação nas abas de dúvidas.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.querySelectorAll()
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle('ativa')
+    })
+})
